52f8b5f744c2918498ea2e7170cfe85dfbfc8391
Alex Ptakhin <alex.ptakhin@prestatech.com>
2024-02-02 17:11:25 +0100

    Initial interfaces

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+```bash
+PYTHONPATH=. pytest-watcher --now .
+```
---
+++ tests/test_general.py
@@ -0,0 +1,27 @@
+import uuid
+from dataclasses import dataclass
+
+import pytest
+
+from hurrqueue import InMemoryConnector, Queue
+
+
+@dataclass
+class Message(object):
+    message_id: uuid.UUID
+
+
+class MessageQueue(Queue[Message]):
+    pass
+
+
+@pytest.mark.asyncio()
+async def test_basic() -> None:
+    q = MessageQueue(connector=InMemoryConnector(storage={}), name='inmemory')
+    msg = Message(message_id=uuid.uuid4())
+    await q.put(msg)
+
+    read_msg = await q.pull()
+    assert read_msg
+
+    assert read_msg.message_id == msg.message_id
